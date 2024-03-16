6dc7ed75db9d16a4e6ddfac3178c56ea47262b1f
mwanyasi <jmwanyambu@gmail.com>
2024-03-16 08:17:40 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,20 +7,14 @@ def canUnlockAll(boxes):
     check if all boxes can be unlocked
     """
 
-    if not boxes:
+    if not boxes or type(boxes) is not list:
         return False
 
-    size = len(boxes)
-    x = 0
-    y = {}
-
-    for box in boxes:
-        if len(box) == 0 or x == 0:
-            y[x] = -1
-        for key in box:
-            if key < size and key != x:
-                y[key] = key
-        if len(y) == size:
-            return True
-        x += 1
+    ubox = [0]
+    for i in ubox:
+        for key in boxes[i]:
+            if key not in ubox and key < len(boxes):
+                ubox.append(key)
+    if len(ubox) == len(boxes):
+        return True
     return False

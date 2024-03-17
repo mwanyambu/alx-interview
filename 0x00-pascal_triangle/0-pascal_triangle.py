0e1f5810446d6359d816162b3a56ab77a315e1e2
mwanyasi <jmwanyambu@gmail.com>
2024-03-17 20:42:39 +0000

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for x in range(n):
+        row = [1] * (x + 1)
+        if x > 1:
+            for y in range(1, len(row) - 1):
+                row[y] = triangle[x - 1][y - 1] + triangle[x - 1][y]
+        triangle.append(row)
+    return triangle

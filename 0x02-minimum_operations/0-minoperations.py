7a748036063adc07bed4b760dda06538a69b7986
mwanyambu <jmwanyambu@gmail.com>
2024-03-24 18:54:17 +0000

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""minimum operations"""
+
+
+def minOperations(n):
+    """
+    use minimum operation to calculate
+    """
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops

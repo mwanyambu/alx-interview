f4ec85c592079b617ade2fa0e72f8c79cf1a250c
mwanyambu <jmwanyambu@gmail.com>
2024-04-04 20:39:41 +0000

    utf-8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,6 +8,24 @@ def validUTF8(data):
     method that determines if a given data set represents valid UTF-8 encoding
     """
     x = 0
+    for byte in data:
+        if x == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                x = 1
+            elif byte >> 4 == 0b1110:
+                x = 2
+            elif byte >> 3 == 0b11110:
+                x = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            x -= 1
+    return x == 0
+    """
     while x < len(data):
         bytess = 0
         mask = 0b10000000
@@ -28,3 +46,4 @@ def validUTF8(data):
         x += bytess
 
     return True
+    """

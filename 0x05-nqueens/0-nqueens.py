8b899abf1f5af9bddad6d3c5ff796fe35ecc4d7d
mwanyambu <jmwanyambu@gmail.com>
2024-04-14 22:59:24 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,42 +2,83 @@
 """ nqueens """
 import sys
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    sys.exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-N = int(sys.argv[1])
+solution = []
+n = 0
+ind = None
 
 
-def nQueens(N, row=0, a=[], b=[], c=[]):
-    """ nQueens """
-    if row < N:
-        for col in range(N):
-            if col not in a and row+col not in b and row-col not in c:
-                yield from nQueens(N, row+1, a+[col], b+[row+col], c+[row-col])
+def input_validation():
+    """ valudate user input"""
+    global n
+    n = 0
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print(" must be atlearst 4")
+        sys.exit(1)
+    return n
+
+
+def check_position(row, col):
+    """ check position of queen"""
+    if (row[0] == col[0] or (row[1] == col[1])):
+        return True
+    return abs(row[0] - col[0]) == abs(row[1] - col[1])
+
+
+def check_group(group):
+    """ check if group exists in solution"""
+    global solution
+    for s in solution:
+        x = 0
+        for t in s:
+            for g in group:
+                if t[0] == g[0] and t[1] == g[1]:
+                    x += 1
+        if x == n:
+            return True
+    return False
+
+
+def get_solution(row, group):
+    """ get solution for nqueens"""
+    global solution
+    global n
+
+    if row == n:
+        tmp = group.copy()
+        if not check_group(tmp):
+            solution.append(tmp)
     else:
-        yield a
+        for col in range(n):
+            a = (row * n) + col
+            matches = zip(list([ind[a]]) * len(group), group)
+            pos = map(lambda x: check_position(x[0], x[1]), matches)
+            group.append(ind[a].copy())
+            if not any(pos):
+                get_solution(row + 1, group)
+            group.pop(len(group) - 1)
 
 
-def solve(N):
-    """ solution """
-    x = []
-    y = 0
-    for solution in nQueens(N):
-        for z in solution:
-            x.append([y, z])
-            y += 1
-        print(x)
-        x = []
-        y = 0
+def print_solution():
+    """ print soluntion """
+    global solution
+    global n
+
+    pos = list(map(lambda x: [x // n, x % n], range(n ** 2)))
+    j = 0
+    group = []
+    get_solution(0, group)
 
 
-solve(N)
+n = input_validation()
+print_solution()
+for s in solution:
+    print(s)
